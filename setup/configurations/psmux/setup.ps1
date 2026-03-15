8fdf4cd58cca7537c8d6d709adfa0b54fe870b98
Showen <showenotto@proton.me>
2026-03-15 18:27:27 +0200

    1.0

--- setup/configurations/psmux/setup.ps1
+++ setup/configurations/psmux/setup.ps1
@@ -4,8 +4,8 @@ $user = "Showen"
 $psmuxBackupFile = "C:\Users\$user\windows11-build\setup\configurations\psmux\psmux.conf"  # Your backup file
 $psmuxTargetDir = "C:\Users\$user\"
 
-$pshBackupFile = "C:\Users\$user\windows11-build\setup\configurations\psmux\Microsoft.PowerShell_profile.ps1"
-$pshTargetDir = "C:\Users\Showen\Documents\PowerShell\"
+$pshBackupFile = "C:\Users\$user\windows11-build\setup\configurations\psmux\Microsoft.PowerShell_profile.ps1.config"
+$pshTargetDir = "C:\Users\Showen\Documents\WindowsPowerShell\"
 
 Write-Host "Restoring PSMUX configuration..." -ForegroundColor Cyan
 
@@ -14,6 +14,6 @@ if (-not (Test-Path $pshTargetDir))
 }
 
 Copy-Item -Path $psmuxBackupFile -Destination "$psmuxTargetDir\.psmux.conf" -Recurse -Force
-Copy-Item -Path $pshBackupFile -Destination $pshTargetDir -Recurse -Force
+Copy-Item -Path $pshBackupFile -Destination "$pshTargetDir\Microsoft.PowerShell_profile.ps1" -Recurse -Force
 
 Write-Host "`nDone!" -ForegroundColor Green
